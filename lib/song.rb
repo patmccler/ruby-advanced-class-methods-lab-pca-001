5b2f5b1d6c85a425beda8dca4af0b3ed7082e84f
Pat McClernan <patmccler@gmail.com>
2020-05-06 09:14:10 -0400

    Implements methods for song class

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = new
+    song.save
+    song
+  end
+
+  def self.new_by_name name
+    song = new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name name
+    song = new_by_name name
+    song.save
+    song
+  end
+
+  def self.find_by_name name
+    all.find{ |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name name
+    (find_by_name name) || (create_by_name name)
+  end
+
+  def self.alphabetical
+    all.sort_by(&:name)
+  end
+
+  def self.new_from_filename(filename)
+    song = create_by_name get_name_from_filename(filename)
+    song.artist_name = get_artist_from_filename filename
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = new_from_filename filename
+    song.save
+    song
+  end
+
+  def self.get_artist_from_filename(filename)
+    /^(.*)\s\-/.match(filename).captures.first
+  end
+
+  def self.get_name_from_filename(filename)
+    /\-\s(.*)\./.match(filename).captures.first
+  end
+
+  def self.destroy_all
+    all.clear
+  end
 end
